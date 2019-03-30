25cf28ea7f5265d5d97f0755a76c13b8ab3ea378
JazHar <jasonharris06@gmail.com>
2019-03-30 12:28:51 -0600

    working cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import Header from "./components/Header";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    score: 13,
+    highscore: 0
+  };
+
+ //Game Logic
+  // gameLogic = () => {
+  //   this.state.friends.
+  // }
+
+//handleClick needs to grab the of the item clicked, changes the score if the 
+  handleClick = (id) => {
+    console.log(id);
+     let friends = this.state.friends;
+     friends = friends.map(friend => {
+       if(friend.id === id ){
+        friend.clicked = true
+       }
+      return friend
+      })
+      //Shuffle Friends
+      friends = this.shuffle(friends);
+      this.setState({friends})
+
+      //Run GameLogic
+      this.gameLogic(friends);
+
+      //  if(id === )
+         console.log(friends);
+      
+  
+
+  };
+
+   //Shuffle the cards after each click 
+   shuffle = (a)=> {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+  
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}></Header>
+        <Title>Clicky Game</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+            click={this.handleClick}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
